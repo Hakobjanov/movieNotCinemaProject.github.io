73c3c4631bb367f44e00bbf54fcff517fc50c579
vahan <vahan.bclub@gmail.com>
2019-12-12 10:57:49 +0300

    movie blog project kinda

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.intro-slider').slick({
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+  });
+
+  $(document).ready(function(){
+    $('.com-slider').slick({
+        dots: true,
+        autoplay:true,
+	 autoplaySpeed: 3700
+     
+    });
+  });
+
+  $(document).ready(function(){
+    $('.space-slider').slick({
+        dots: true,
+        autoplay:true,
+	autoplaySpeed: 3275
+     
+    });
+  });
